974744a24aa8ab63ab9e49a2404657f9e73bf09a
Wagner Bugs <wagnerbugs@gmail.com>
2025-05-27 11:48:45 -0300

    feat: plataform relations

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Store extends BaseModel
@@ -13,10 +14,16 @@ class Store extends BaseModel
     protected $fillable = [
         'name',
         'slug',
+        'platform_id',
     ];
 
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function platform(): BelongsTo
+    {
+        return $this->belongsTo(Platform::class);
     }
 }
--- database/migrations/2025_05_27_002226_create_stores_table.php
+++ database/migrations/2025_05_27_002226_create_stores_table.php
@@ -17,6 +17,7 @@ return new class () extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
+            $table->foreignId('platform_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
 

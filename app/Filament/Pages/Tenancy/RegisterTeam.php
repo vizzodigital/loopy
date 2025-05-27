85fc3d6bd6b26cb1c0dd14326815f8e9e64dd1d7
Wagner Bugs <wagnerbugs@gmail.com>
2025-05-27 09:11:44 -0300

    feat: plataform

--- app/Filament/Pages/Tenancy/RegisterTeam.php
+++ app/Filament/Pages/Tenancy/RegisterTeam.php
@@ -5,9 +5,12 @@ declare(strict_types = 1);
 namespace App\Filament\Pages\Tenancy;
 
 use App\Models\Store;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Pages\Tenancy\RegisterTenant;
+use Illuminate\Support\Str;
 
 class RegisterTeam extends RegisterTenant
 {
@@ -21,7 +24,13 @@ class RegisterTeam extends RegisterTenant
         return $form
             ->schema([
                 TextInput::make('name')
-                    ->label('Loja'),
+                    ->label('Loja')
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
+
+                Select::make('store_id')
+                    ->options(Store::all()->pluck('name', 'id')),
+
                 TextInput::make('slug')
                     ->unique('stores', 'slug'),
             ]);

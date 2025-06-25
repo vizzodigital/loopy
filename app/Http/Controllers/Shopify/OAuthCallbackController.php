4dfd1eee26a9ab73a051555457d6c5c9e6f82aeb
Wagner Bugs <wagnerbugs@gmail.com>
2025-06-25 10:37:07 -0300

    fix: v4

--- app/Http/Controllers/Shopify/OAuthCallbackController.php
+++ app/Http/Controllers/Shopify/OAuthCallbackController.php
@@ -26,9 +26,9 @@ class OAuthCallbackController extends Controller
         $params = $request->except(['hmac']);
         ksort($params);
         $queryString = urldecode(http_build_query($params));
-        
+
         $calculatedHmac = hash_hmac('sha256', $queryString, (string) config('services.shopify.client_secret'));
-        dd($queryString, $hmac, $calculatedHmac);
+
         if (!hash_equals($hmac, $calculatedHmac)) {
             return response('HMAC validation failed', 400);
         }

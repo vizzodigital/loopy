92438c9eb9dfa43f1e100175e7a91fe40de5a711
Wagner Bugs <wagnerbugs@gmail.com>
2025-06-25 17:17:44 -0300

    feat: cookies

--- app/Http/Controllers/Shopify/OAuthAccessTokenController.php
+++ app/Http/Controllers/Shopify/OAuthAccessTokenController.php
@@ -33,7 +33,12 @@ class OAuthAccessTokenController extends Controller
             return response('HMAC validation failed', 400);
         }
 
-        $response = Http::post("https://{$shop}/admin/oauth/access_token", [
+        $integration = Integration::whereJsonContains('configs->shop', $shop)->first();
+
+        $response = Http::withHeaders([
+            'Cookie' => 'state=' . $integration->webhook,
+        ])
+        ->post("https://{$shop}/admin/oauth/access_token", [
             'client_id' => config('services.shopify.client_id'),
             'client_secret' => config('services.shopify.client_secret'),
             'code' => $code,

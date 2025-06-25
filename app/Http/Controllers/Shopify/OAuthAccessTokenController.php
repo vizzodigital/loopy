4e5881400f53cafc0e980cf85c60912c8b6a323d
Wagner Bugs <wagnerbugs@gmail.com>
2025-06-25 11:27:43 -0300

    fix: v8

--- app/Http/Controllers/Shopify/OAuthAccessTokenController.php
+++ app/Http/Controllers/Shopify/OAuthAccessTokenController.php
@@ -6,14 +6,59 @@ namespace App\Http\Controllers\Shopify;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class OAuthAccessTokenController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        //
+        dd($request->all());
+        $code = $request->query('code');
+        $hmac = $request->query('hmac');
+        $shop = $request->query('shop');
+        $timestamp = $request->query('timestamp');
+
+        $params = $request->except(['hmac']);
+        ksort($params);
+        $queryString = urldecode(http_build_query($params));
+
+        $calculatedHmac = hash_hmac('sha256', $queryString, (string) config('services.shopify.client_secret'));
+
+        if (!hash_equals($hmac, $calculatedHmac)) {
+            return response('HMAC validation failed', 400);
+        }
+        $response = Http::post("https://{$shop}/admin/oauth/access_token", [
+            'client_id' => config('services.shopify.client_id'),
+            'client_secret' => config('services.shopify.client_secret'),
+            'code' => $code,
+        ]);
+
+        // if (!$response->ok()) {
+        //     Log::error('Shopify token error', ['response' => $response->body()]);
+
+        //     return response('Token request failed', 500);
+        // }
+
+        // $data = $response->json();
+
+        // Log::info('Shopify installed', [
+        //     'shop' => $shop,
+        //     'access_token' => $data['access_token'],
+        //     'scope' => $data['scope'],
+        // ]);
+
+        // $integration = Integration::where('store_id', Filament::getTenant()->id)
+        //     ->where('platform_id', 2)
+        //     ->where('type', IntegrationTypeEnum::ECOMMERCE)
+        //     ->first();
+
+        // $integration->update([
+        //     'configs' => [
+        //         'shop' => $data['scope'],
+        //         'access_token' => $data['access_token'],
+        //         'scope' => $data['scope'],
+        //     ],
+        //     'is_active' => true,
+        // ]);
     }
 }

782202510748f49a2d22e03973e21dd1b21b049b
Victor Miguez <victorcineis@gmail.com>
2025-06-28 10:52:57 +1000

    fix: update entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-echo "📦 Installing Composer dependencies..."
-composer install --no-dev --optimize-autoloader
+# echo "📦 Installing Composer dependencies..."
+# composer install --no-dev --optimize-autoloader
+
+echo "⚙️ Building assets with Vite..."
+npm install --legacy-peer-deps
+npm run build
 
 echo "🔐 Generating app key..."
 php artisan key:generate || true
 
 echo "🧬 Running migrations..."
 php artisan migrate || true
-
-echo "⚙️ Building assets with Vite..."
-npm install --legacy-peer-deps
-npm run build
